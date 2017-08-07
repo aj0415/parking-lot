42ce5c7bcb73c59a4f3e8b316ab26bcc376cb584
Aaron Herbert <aj0415@me.com>
2017-08-07 15:30:20 -0400

    Finish revenue and moving from queue to lot when space clears

--- parking/app.py
+++ parking/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 from werkzeug.exceptions import NotFound
 
-from parking.db.parking import get_lot_status, get_queue_status, leave, park
+from parking.db.parking import get_lot_status, get_queue_status, get_revenue_status, leave, park
 
 app = Flask(__name__)
 
@@ -10,14 +10,14 @@ app = Flask(__name__)
 def lot_status():
     try:
         return render_template('status.html', lot_data=get_lot_status(),
-                               queue_data=get_queue_status())
+                               queue_data=get_queue_status(), revenue_data=get_revenue_status())
     except AttributeError:
         raise NotFound
 
 
 @app.route('/park/<vehicle_name>/<parking_time_length>', methods=['POST'])
 def park_vehicle(vehicle_name, parking_time_length):
-    park(vehicle_name, parking_time_length)
+    park(vehicle_name, int(parking_time_length))
     return '', 202
 
 
