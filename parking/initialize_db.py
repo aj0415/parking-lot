f2bc81913dbaaa36b953897197bb519fb6dee8a9
Aaron Herbert <aj0415@me.com>
2017-08-08 23:01:51 -0400

    Fix initialize-db command

---
+++ parking/initialize_db.py
@@ -0,0 +1,10 @@
+import parking.db.parking
+
+
+def initialize_db():
+    parking.db.parking.metadata.create_all()
+    parking.db.parking.initialize()
+
+
+if __name__ == '__main__':
+    initialize_db()
--- setup.py
+++ setup.py
@@ -1,8 +1,6 @@
-from setuptools import os, setup, find_packages
-
-db_command = os.path.join('bin', 'initialize-db')
+from setuptools import setup, find_packages
 
 setup(name='parking', version='0.1', packages=find_packages(),
-      scripts=[db_command],
-      entry_points={'console_scripts': ['parking-serve=parking.app:app.run',
+      entry_points={'console_scripts': ['initialize-db=parking.initialize_db:initialize_db',
+                                        'parking-serve=parking.app:app.run',
                                         'parking-remove-db=parking.db.parking:drop_db']})
