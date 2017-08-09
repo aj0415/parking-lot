c612dd8c5bcef2ef7d3f514a18a10e3ce2445fbf
Aaron Herbert <aj0415@me.com>
2017-08-08 22:33:53 -0400

    Attempt to fix db initialize script for Windows

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
-from setuptools import setup, find_packages
+from setuptools import os, setup, find_packages
+
+db_command = os.path.join('bin', 'initialize-db')
 
 setup(name='parking', version='0.1', packages=find_packages(),
-      scripts=['initialize-db'],
+      scripts=[db_command],
       entry_points={'console_scripts': ['parking-serve=parking.app:app.run',
                                         'parking-remove-db=parking.db.parking:drop_db']})
